8d16514f18049d1d498a2448cd64543633256ffb
GOBIKRISHNAS <76651403+GOBIKRISHNAS@users.noreply.github.com>
2021-02-24 18:44:47 +0530

    Update javascript.js

--- user-page/javascripts/javascript.js
+++ user-page/javascripts/javascript.js
@@ -2,9 +2,20 @@ var user_name = localStorage.getItem("userName");
 
 var books_list = [
     {name: "Eating in the Age of Dieting", author: "Rujuta Diwekar", cost: "₹265.00", books_available: 2, img_url: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg"},
-    {name: "The Complete Novels of Sherlock Holmes", author: "Arthur Conan Doyle", cost: "₹149", books_available: 4, img_url: "https://images-eu.ssl-images-amazon.com/images/I/41tT5SJp6+L._AC_SX184_.jpg"},
+    {name: "Novels of Sherlock Holmes", author: "Arthur Conan Doyle", cost: "₹149", books_available: 4, img_url: "https://images-eu.ssl-images-amazon.com/images/I/41tT5SJp6+L._AC_SX184_.jpg"},
     {name: "The Secret Garden", author: "Frances Hodgson Burnett", cost: "₹124.00", books_available: 5, img_url: "https://images-eu.ssl-images-amazon.com/images/I/51V-OwTiNVL._AC_SX184_.jpg"},
-    {name: "Black 13 (Scott Pearce)", author: "Adam Hamdy", cost: "₹333.00", books_available: 7, img_url: "https://images-eu.ssl-images-amazon.com/images/I/419Y6Jy1OUL._AC_SX184_.jpg"}
+    {name: "Black 13 (Scott Pearce)", author: "Adam Hamdy", cost: "₹333.00", books_available: 7, img_url: "https://images-eu.ssl-images-amazon.com/images/I/419Y6Jy1OUL._AC_SX184_.jpg"},
+    {name: "Such a Fun Age", author: "Kiley Reid", cost: "₹388", books_available: 3,img_url: "https://images-eu.ssl-images-amazon.com/images/I/51TEBoy4lnL._AC_SX184_.jpg"},
+    {name: "Joe Biden: American Dreamer", author: "Evan Osnos", cost: "₹559", books_available: 2,img_url: "https://images-eu.ssl-images-amazon.com/images/I/414+86apq9L._AC_SX184_.jpg"},
+    {name: "Girl A: The Sunday Times", author: "Kiley Reid", cost: "₹388", books_available: 3,img_url: "https://images-eu.ssl-images-amazon.com/images/I/41+r9c1dkYL._AC_SX184_.jpg"},
+    {name: "The Bengal Conundrum", author: "Kiley Reid", cost: "₹388", books_available: 4,img_url: "https://images-eu.ssl-images-amazon.com/images/I/41b7XHWxcnL._AC_SX184_.jpg"},
+    {name: "The Presidential Years: 2012-2017", author: "Abigail Dean", cost: "₹388", books_available: 6,img_url: "https://images-eu.ssl-images-amazon.com/images/I/5163PKxRQAL._AC_SX184_.jpg"},
+    {name: "1984 Nineteen Eighty-Four ", author: "Kiley Reid", cost: "₹388", books_available: 2,img_url: "https://images-eu.ssl-images-amazon.com/images/I/515DZG1Jd6L._AC_SX184_.jpg"},
+    {name: "Superheavy: Making and Breaking", author: "Evan Osnos", cost: "₹388", books_available: 7,img_url: "https://images-eu.ssl-images-amazon.com/images/I/51myTw8aK8L._AC_SX184_.jpg"},
+    {name: "Yes Man: The Untold Story", author: "Kiley Reid", cost: "₹388", books_available: 9,img_url: "https://images-eu.ssl-images-amazon.com/images/I/41zOkw9vLFL._AC_SX184_.jpg"},
+    {name: "Dreamers and Unicorns", author: "Arthur Conan Doyle", cost: "₹388", books_available: 12,img_url: "https://images-eu.ssl-images-amazon.com/images/I/41eESmdS3IL._AC_SX184_.jpg"},
+    {name: "Brand New Start: Fast-Start", author: "Kiley Reid", cost: "₹388", books_available: 6,img_url: "https://images-eu.ssl-images-amazon.com/images/I/51W70enRUdL._AC_SX184_.jpg"},
+    {name: "Hidden in Plain Sight", author: "Arthur Conan Doyle", cost: "₹388", books_available: 8,img_url: "https://images-eu.ssl-images-amazon.com/images/I/512+dA1m+EL._AC_SX184_.jpg"},
 ]
 
 var new_arrivals = [
@@ -14,7 +25,7 @@ var new_arrivals = [
 
 var books_borrowed = {
     krishna: [
-        {name: "The Complete Novels of Sherlock Holmes", author: "Arthur Conan Doyle", cost: "₹149", date_of_return: "01/03/2021", date_of_borrow: "15/02/2021", img_url: "https://images-eu.ssl-images-amazon.com/images/I/41tT5SJp6+L._AC_SX184_.jpg"}
+        {name: "Novels of Sherlock Holmes", author: "Arthur Conan Doyle", cost: "₹149", date_of_return: "01/03/2021", date_of_borrow: "15/02/2021", img_url: "https://images-eu.ssl-images-amazon.com/images/I/41tT5SJp6+L._AC_SX184_.jpg"}
     ],
     abirami: [
         {name: "Eating in the Age of Dieting", author: "Rujuta Diwekar", cost: "₹265.00",  date_of_return: "01/03/2021", date_of_borrow: "15/02/2021", img_url: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg"}
@@ -43,7 +54,7 @@ function availablebooks() {
 
     if (books_list.length>0){
         for (i = 0; i < books_list.length; i++) {
-            str = str + "<li><img src = '"+ books_list[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + books_list[i].name + "</h1><p>" + books_list[i].author +"</p><p>" + books_list[i].cost + "</p><p>Books Available: " + books_list[i].books_available + "</p><input type='button' value='Borrow Now'></div></li>"
+            str = str + "<li><a href='#'><img src = '"+ books_list[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + books_list[i].name + "</h1><p>" + books_list[i].author +"</p><p>" + books_list[i].cost + "</p><p>Books Available: " + books_list[i].books_available + "</p></div></a></li>"
         } 
     }
     else{
@@ -63,7 +74,7 @@ function newarrivals() {
 
     if (new_arrivals.length>0){
         for (i = 0; i < new_arrivals.length; i++) {
-            str = str + "<li><img src = '"+ new_arrivals[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + new_arrivals[i].name + "</h1><p>" + new_arrivals[i].author +"</p><p>" + new_arrivals[i].cost + "</p><p>Books Available: " + new_arrivals[i].books_available + "</p><input type='button' value='Borrow Now'></div></li>"
+            str = str + "<li><a href='#'><img src = '"+ new_arrivals[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + new_arrivals[i].name + "</h1><p>" + new_arrivals[i].author +"</p><p>" + new_arrivals[i].cost + "</p><p>Books Available: " + new_arrivals[i].books_available + "</p></div></a></li>"
         } 
     }
     else{
@@ -98,29 +109,19 @@ function booksborrowed() {
     document.getElementById("searchtab").style.display = "none";
 }
 
-function search() {
-    var search_variable = document.getElementsByName("search")[0].value;
-
-    str = ''
-    if (books_list.length>0){
-        for (i = 0; i < books_list.length; i++) {
-            string = (books_list[i].name).toLowerCase()
-            author_string = (books_list[i].author).toLowerCase()
-            if(string.includes(search_variable) == true || author_string.includes(search_variable) == true){
-                str = str + "<li><img src = '"+ books_list[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + books_list[i].name + "</h1><p>" + books_list[i].author +"</p><p>" + books_list[i].cost + "</p><p>Books Available: " + books_list[i].books_available + "</p><input type='button' value='Borrow Now'></div></li>"
-            }
+function myFunction() { 
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("content1");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
         }
-    } else{
-        str = "No Books Available"
     }
-    
-    document.getElementById("content4").innerHTML = str
-    document.getElementById("dashboard").style.display = "none";
-    document.getElementById("availablebooks").style.display = "none";
-    document.getElementById("newarrivals").style.display = "none";
-    document.getElementById("booksborrowed").style.display = "none";
-    document.getElementById("searchtab").style.display = "block";
-
 }
-
-    
